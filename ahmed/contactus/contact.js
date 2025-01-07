429f92ce0955f075cc896489e60d4fddc59be42c
modytr <modytr71@gmail.com>
2025-01-07 18:05:51 +0300

    Update contact.js

--- ahmed/contactus/contact.js
+++ ahmed/contactus/contact.js
@@ -1,9 +1,9 @@
 function hoverLabel(inputId, isHover) {
       const label = document.querySelector(`label[for=${inputId}]`);
       if (isHover) {
-          label.style.color = '#33d1ca'; // Change to your desired color
+          label.style.color = '#33d1ca';
       } else {
-          label.style.color = ''; // Reset to default color
+          label.style.color = '';
       }
   }
 
